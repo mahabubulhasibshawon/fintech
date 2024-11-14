a1be6c6b2ce763bdd8fc0a2b696270581502a593
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-11-14 23:34:15 +0600

    btn

--- lib/screens/email_verification.dart
+++ lib/screens/email_verification.dart
@@ -1,24 +1,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class EmailVerification extends StatelessWidget {
+class EmailVerification extends StatefulWidget {
   const EmailVerification({super.key});
+
+  @override
+  State<EmailVerification> createState() => _EmailVerificationState();
+}
+
+class _EmailVerificationState extends State<EmailVerification> {
+  bool isReminderChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: GestureDetector(child: Image.asset('assets/images/back_btn.png'),),
+        leading: GestureDetector(
+          child: Image.asset('assets/images/back_btn.png'),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Let's verify your email", style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),),
+            Text(
+              "Let's verify your email",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
             TextField(
+              keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                hintStyle: TextStyle(color: Colors.grey),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
-            )
+            ),
+            Spacer(),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Checkbox(
+                  value: isReminderChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isReminderChecked = value ?? false;
+                    });
+                  },
+                ),
+                Expanded(
+                  child: Text(
+                    "I prefer email as my primary form of feedback and contact",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your action here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF056232),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  'Verify Email',
+                  style: TextStyle(color: Colors.white,fontSize: 16),
+                ),
+              ),
+            ),
           ],
         ),
       ),
