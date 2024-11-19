8075d54abeceb367d77e48c8d933542f685fdf5a
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-11-19 22:40:56 +0600

    cart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,10 +21,26 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            height: Height * .15,
-            width: Width * .8,
-            color: Colors.white,
+          Center(
+            child: Container(
+              height: Height * .2,
+              width: Width * .8,
+              decoration: BoxDecoration(
+                color: Color(0xff2c135f),
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Text('147570', style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold, color: Colors.white),),
+                      Text('Mahabub Ul Hasib Shawon', style: TextStyle(fontSize: 24, color: Colors.white),),
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
